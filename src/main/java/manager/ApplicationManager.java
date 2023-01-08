edd662857170954cc7832cf639084ecd386213ee
AlexAksioma <alexmedqwerty@gmail.com>
2023-01-08 17:52:31 +0200

    Home work searh tests

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -38,7 +38,7 @@ public class ApplicationManager {
             wd = new EventFiringWebDriver(new FirefoxDriver());
         wd.register(new MyListener());
         wd.manage().window().maximize();
-        //wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wd.navigate().to("https://ilcarro.web.app/");
         user = new HelperUser(wd);
         registration = new HelperRegistration(wd);
